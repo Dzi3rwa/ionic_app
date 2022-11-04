f413c74764cd6c62bdb183b39ae507b6599619df
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-11-04 12:57:27 +0100

    get position

--- ionic-first-app/src/app/tab1/tab1.page.ts
+++ ionic-first-app/src/app/tab1/tab1.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
+  currentPositionTab: any[] = [];
+  currentPositionTab2: any[] = [];
+  bool = false;
+  constructor() {
+    setInterval(() => {
+      this.printCurrentPosition();
+    }, 5000);
+  }
 
-  constructor() {}
+  printCurrentPosition = async () => {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.currentPositionTab.push(coordinates);
+  };
 
+  buttonClick() {
+    this.currentPositionTab2.length = 0;
+    this.currentPositionTab.forEach(e => {
+      this.currentPositionTab2.push(e);
+    });
+    this.currentPositionTab.length = 0;
+    this.bool = true;
+  }
 }
