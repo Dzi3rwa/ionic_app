a452feb924ee61414b0ac5ea72247dbe844f970b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-11-15 23:52:21 +0100

    aa

--- ionic-first-app/src/app/tab1/tab1.page.ts
+++ ionic-first-app/src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Geolocation } from '@capacitor/geolocation';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-tab1',
@@ -11,15 +12,27 @@ export class Tab1Page {
   currentPositionTab: any[] = [];
   currentPositionTab2: any[] = [];
   bool = false;
-  constructor() {
+  length = 0;
+  constructor(private storage: Storage) {
     setInterval(() => {
       this.printCurrentPosition();
     }, 5000);
-  }
+    this.ngOnInit();
+  };
+
+  ngOnInit = async () => {
+    await this.storage.create();
+    this.length = await this.storage.length();
+  };
 
   printCurrentPosition = async () => {
     const coordinates = await Geolocation.getCurrentPosition();
-    this.currentPositionTab.push(coordinates);
+    const x = JSON.stringify(coordinates.coords.latitude);
+    const y = JSON.stringify(coordinates.coords.longitude);
+    const obj = { x, y };
+    this.currentPositionTab.push(obj);
+    await this.storage.set(JSON.stringify(obj), 'a');
+    this.length = await this.storage.length();
   };
 
   buttonClick() {
@@ -29,5 +42,10 @@ export class Tab1Page {
     });
     this.currentPositionTab.length = 0;
     this.bool = true;
-  }
+  };
+
+  buttonDeleteClick = async () => {
+    await this.storage.clear();
+    this.length = await this.storage.length();
+  };
 }
